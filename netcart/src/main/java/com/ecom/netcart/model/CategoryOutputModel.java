38a83dedab766e00ee30d3ddc6752677f5f1012e
Ishita Singh <92878405+issue31@users.noreply.github.com>
2024-12-04 04:07:28 +0530

    Delete netcart directory

--- netcart/src/main/java/com/ecom/netcart/model/CategoryOutputModel.java
+++
@@ -1,9 +0,0 @@
-package com.ecom.netcart.model;
-
-import lombok.Data;
-
-@Data
-public class CategoryOutputModel {
-	private String categoryDescription;
-private String categoryTitle;
-}
--- netcart/src/main/java/com/ecom/netcart/model/UserLoginInfo.java
+++
@@ -1,9 +0,0 @@
-package com.ecom.netcart.model;
-
-import lombok.Data;
-
-@Data
-public class UserLoginInfo {
-private String  email;
-private String password;
-}
